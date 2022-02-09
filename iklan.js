0f574fb71eff7f6fe747692c2a1b6f1e5053a631
railyemb <98513464+railyemb@users.noreply.github.com>
2022-02-09 15:04:46 +0700

    Create iklan.js
    iklan

---
+++ iklan.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '9f1228b8bd488f417fb90b89cf6f26f2',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://functionssubqueries.com/9f1228b8bd488f417fb90b89cf6f26f2/invoke.js"></scr' + 'ipt>');
